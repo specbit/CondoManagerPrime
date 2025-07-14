3862af13911b0eead002c68489e91efa846ef18e
specbit <nmiguelgomes77@gmail.com>
2025-07-14 18:28:04 +0100

    Add project files.

---
+++ CET96_ProjetoFinal.web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using CET96_ProjetoFinal.web.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CET96_ProjetoFinal.web.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Additional model configurations can be added here
+        }
+        // DbSet properties for your entities can be added here, e.g.:
+        // public DbSet<Product> Products { get; set; }
+    }
+}
